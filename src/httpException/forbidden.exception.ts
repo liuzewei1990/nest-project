9c14a9f3e7016cdbe2a66a2b2b54b16013969047
冰河 <543347844@qq.com>
2018-04-26 22:17:45 +0800

    异常处理

---
+++ src/httpException/forbidden.exception.ts
@@ -0,0 +1,11 @@
+import { HttpException } from "@nestjs/core";
+import { HttpStatus } from "@nestjs/common";
+
+export class ForbiddenHttpException extends HttpException{
+    constructor(){
+        super({
+            resultCode:HttpStatus.FORBIDDEN,
+            resultMsg:"继承HttpException抛出的自定义异常"
+        },HttpStatus.FORBIDDEN)
+    }
+}
--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, HttpStatus, RequestTimeoutException, NotFoundException } from "@nestjs/common";
 import { AdService } from "../ad/ad.service";
+import { HttpException } from "@nestjs/core";
+import { ForbiddenHttpException } from "httpException/forbidden.exception";
 
 @Controller("user")
 export class UsersController {
@@ -9,5 +11,24 @@ export class UsersController {
   @Get()
   findUsers(): object {
     return this.adService.findAds();
+  }
+
+  @Get("error") exception(){
+
+    //自定义异常一
+    // throw new HttpException("这是服务器手动抛出的异常错误",HttpStatus.FORBIDDEN);
+
+    //自定义异常二
+    // throw new HttpException({
+    //   resultCode:HttpStatus.FORBIDDEN,
+    //   resultMsg:"这是服务器手动抛出的异常错误"
+    // },HttpStatus.FORBIDDEN);
+
+    //使用继承HttpException抛出异常
+    // throw new ForbiddenHttpException();
+
+    //nest内置异常
+    // throw new RequestTimeoutException()
+    throw new NotFoundException();
   }
 }
