a1dbb945a560569d4f629a75c79b567a040bdcd0
zewei <543347844@qq.com>
2018-04-27 20:35:42 +0800

    修改端口号

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ApplicationModule  } from './module/app.module';
+import { ApplicationModule } from './module/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule );
-  await app.listen(3000);
+  const app = await NestFactory.create(ApplicationModule);
+  await app.listen(8070);
 }
 bootstrap();
